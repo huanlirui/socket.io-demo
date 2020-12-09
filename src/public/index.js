609da57875d8dbbe23b5c8f8aa620680060441ab
huanlirui <472496416@qq.com>
2020-12-09 18:21:13 +0800

    feat(用户列表，聊天室信息。还有bug)

--- src/public/index.js
+++ src/public/index.js
@@ -1,29 +1,73 @@
-const socket = io('/rooms');
+const socketRoomList = io('/rooms');
+const socketChatroom = io('/chatroom');
 const roomContainer = document.getElementById('room-container');
-const roomInput = document.getElementById('roomInput');
+const userContainer = document.getElementById('user-list');
+const roomNameContainer = document.getElementById('room-name');
+const sendForm = document.getElementById('send-container');
+const sendInput = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container')
+var userInfo = {
+  user_id: 123456565656,
+  username: '客户' + 126767673456,
+};
+var currentRoom = {};
+
 addRoomForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  socketRoomList.emit('createRoom', userInfo);
+});
+sendForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = sendInput.value;
+  socketChatroom.emit('newMessage', currentRoom.roomId, message);
+});
 
-  // const roomName = roomInput.value;
-  let userInfo = {
-    user_id: genID(),
-    username: '客户' + genID(),
-  };
-  socket.emit('createRoom', userInfo);
-});
-socket.on('updateRoomsList', (data) => {
-  console.log('data', data);
-  console.log('data', Object.values(data));
+socketRoomList.emit('updateRoomsList', userInfo);
+socketRoomList.on('updateRoomsList', (data) => {
   roomContainer.innerHTML = '';
-  Object.values(data).forEach((item) => {
-    appendRoom(item.name ? item.name : '未命名房间');
-  });
+  for (let key in data) {
+    appendRoom(key, data[key]);
+  }
 });
-function appendRoom(message) {
+socketChatroom.on('addMessage', (data) => {
+  
+  appendMessage(data);
+});
+
+socketChatroom.on('updateUsersList', (users) => {
+  currentRoom.users = users;
+  console.log('users', users);
+  for (let value of users) {
+    appendUser(value.username);
+  }
+});
+
+function appendMessage(message) {
   const messageElement = document.createElement('div');
   messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
+function appendUser(user) {
+  const userElement = document.createElement('div');
+  userElement.innerText = user;
+  userContainer.append(userElement);
+}
+function appendRoom(key, item) {
+  const messageElement = document.createElement('div');
+  const buttonElement = document.createElement('input');
+  buttonElement.type = 'button';
+  buttonElement.value = '与' + item.name + '的聊天';
+  buttonElement.addEventListener('click', joinRoom.bind(this, key, userInfo));
+  messageElement.appendChild(buttonElement);
   roomContainer.append(messageElement);
 }
+function joinRoom(key, userInfo) {
+  console.log('userInfo', userInfo);
+  socketChatroom.emit('join', key, userInfo);
+  currentRoom.roomId = key;
+  roomNameContainer.innerText = `与${userInfo.username}的聊天`;
+}
+
 function genID() {
   return Number(Math.random().toString().substr(3, 6) + Date.now()).toString(36);
 }
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -8,19 +8,69 @@ const ioEvents = function (io: Server) {
     socket.on('createRoom', async function (userInfo) {
       const rooms = (await rClient.tget('im:rooms')) || {};
       const userId = userInfo.user_id;
-      
+
       if (Object.keys(rooms || {}).includes(userId)) {
         // return socket.emit("updateRoomsList", { error: "Room already exists." });
         throw new Error('房间已存在');
       }
       rooms[userId] = {
-        name:userInfo.username,
+        name: userInfo.username,
         users: [userInfo],
       }; //新建房间  房间中可能有多个客服。
 
       await rClient.tset('im:rooms', rooms);
       // 通知该socket下的updateRoomsList更新房间列表
       socket.emit('updateRoomsList', rooms);
+    });
+    socket.on('updateRoomsList', async function (userInfo: object) {
+      const rooms = (await rClient.tget('im:rooms')) || {};
+      socket.emit('updateRoomsList', rooms);
+    });
+  });
+
+  // Chatroom namespace
+  io.of('/chatroom').on('connection', function (socket: Socket) {
+    socket.on('join', async function (roomId: string, userInfo) {
+      const rooms = (await rClient.tget('im:rooms')) || {};
+      console.log('roomId', roomId);
+      const userId = userInfo.user_id;
+      // 当前客服所处的socket通道
+      // userInfo.socket_id = socket.id;
+      let currentRoom;
+      // 如果房间不存在，就以当前操作者的id为房间id进行创建
+      if (!roomId || rooms?.[roomId]) {
+        rooms[userId] = {
+          name: userInfo.username,
+          users: [userInfo],
+        };
+        currentRoom = rooms[userId];
+      } else {
+        currentRoom = rooms[roomId];
+      }
+      if (Array.isArray(currentRoom.users)) {
+        const index = currentRoom.users.findIndex((u) => u.user_id === userId);
+        if (index > -1) {
+          //如果当前操作者已经在此房间待过，那么就更新下 socket 而已
+          currentRoom.users[index] = userInfo;
+        } else {
+          // 否则把自己push进该房间用户
+          currentRoom.users.push(userInfo);
+        }
+      }
+      let users = currentRoom.users;
+      console.log('users', users);
+      socket.join(roomId);
+      //向房间内所有人发送更新通知。更新该房间成员。
+      socket.emit('updateUsersList', users);
+      // socket.broadcast.to(roomId).emit('updateUsersList', users);
+      
+      await rClient.tset('im:rooms', rooms);
+    });
+    socket.on('newMessage', async function (roomId: string, message: string) {
+      console.log('roomId', roomId)
+      console.log('message', message)
+      socket.emit('addMessage',message)
+      // socket.broadcast.to(roomId).emit('addMessage', message);
     });
   });
 };
